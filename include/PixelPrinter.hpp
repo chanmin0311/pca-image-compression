600b893058634c51f77488637bf14ebcbffb9acc
chanmin0311 <ckstj1500@gmail.com>
2026-07-04 03:26:42 +0000

    feat: add PixelPrinter for pixel output formatting

---
+++ include/PixelPrinter.hpp
@@ -0,0 +1,44 @@
+// PixelPrinter.hpp
+#pragma once
+
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+class PixelPrinter
+{
+public:
+    virtual ~PixelPrinter() = default;
+    virtual void print(const cv::Mat& image, int row, int col) const = 0;
+};
+
+class GrayScalePixelPrinter : public PixelPrinter
+{
+public:
+    void print(const cv::Mat& image, int row, int col) const override
+    {
+        uchar pixel = image.at<uchar>(row, col);
+        std::cout << static_cast<int>(pixel) << " ";
+    }
+};
+
+class RGBPixelPrinter : public PixelPrinter
+{
+public:
+    void print(const cv::Mat& image, int row, int col) const override
+    {
+        cv::Vec3b pixel = image.at<cv::Vec3b>(row, col);
+        std::cout << "(" << static_cast<int>(pixel[0]) << " " << static_cast<int>(pixel[1]) << " "
+                  << static_cast<int>(pixel[2]) << ")" << " ";
+    }
+};
+
+class RGBAPixelPrinter : public PixelPrinter
+{
+public:
+    void print(const cv::Mat& image, int row, int col) const override
+    {
+        cv::Vec4b pixel = image.at<cv::Vec4b>(row, col);
+        std::cout << "(" << static_cast<int>(pixel[0]) << " " << static_cast<int>(pixel[1]) << " "
+                  << static_cast<int>(pixel[2]) << " " << static_cast<int>(pixel[3]) << ")" << " ";
+    }
+};
